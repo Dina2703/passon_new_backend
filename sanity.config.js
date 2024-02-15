f81bf2a0dcb9cf36902c508290ed27081808239a
Dinara Idrissova <Dinara.Idrissova@list.ru>
2024-02-14 22:56:55 -0500

    backend setup with sanity

--- sanity.config.js
+++ sanity.config.js
@@ -1,11 +1,11 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import {schemaTypes} from './schemas'
 
 export default defineConfig({
   name: 'default',
-  title: 'My Sanity Project',
+  title: 'passon_new',
 
   projectId: 'oceikpb5',
   dataset: 'production',
